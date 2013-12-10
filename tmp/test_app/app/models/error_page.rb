09f1b2018181d8376c8e581ebedc5f8ac3ca9f7f
Falk Köppe <falk.koeppe@infopark.de>
2013-12-10 13:57:20 +0100

    don’t display breadcrumbs on error page

--- tmp/test_app/app/models/error_page.rb
+++ tmp/test_app/app/models/error_page.rb
@@ -1,7 +1,12 @@
 class ErrorPage < Page
   cms_attribute :headline, type: :string
 
-  # Overrides method +show_in_navigation+ from +Page+.
+  # Overrides method +show_breadcrumbs?+ from +Page+.
+  def show_breadcrumbs?
+    false
+  end
+
+  # Overrides method +show_in_navigation?+ from +Page+.
   def show_in_navigation?
     false
   end
