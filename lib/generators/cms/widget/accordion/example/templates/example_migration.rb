ca7d90bb4cc74449af1562a7a969a97cfda14188
Falk Köppe <falk.koeppe@infopark.de>
2013-12-12 12:42:26 +0100

    add example widgets into the panel

--- lib/generators/cms/widget/accordion/example/templates/example_migration.rb
+++ lib/generators/cms/widget/accordion/example/templates/example_migration.rb
@@ -2,14 +2,44 @@ class CreateAccordionWidgetExample < ::RailsConnector::Migration
   def up
     homepage = Obj.find_by_path('<%= example_cms_path %>')
 
-    panel_widget1_id = add_widget_to_widget_pool(homepage, '<%= panel_attribute %>', {
-      _obj_class: '<%= panel_obj_class_name %>',
-      headline: 'Panel 1',
+    text_widget1_id = add_widget_to_widget_pool(homepage, {
+      _obj_class: 'TextWidget',
+      content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
     })
 
-    panel_widget2_id = add_widget_to_widget_pool(homepage, '<%= panel_attribute %>', {
+    text_widget2_id = add_widget_to_widget_pool(homepage, {
+      _obj_class: 'TextWidget',
+      content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+    })
+
+    panel_widget1_id = add_widget_to_widget_pool(homepage, {
       _obj_class: '<%= panel_obj_class_name %>',
-      headline: 'Panel 2',
+      headline: 'Duis aute irure dolor in reprehenderit',
+      main_content: {
+        'list' => [
+          { widget: text_widget1_id },
+        ]
+      },
+    })
+
+    panel_widget2_id = add_widget_to_widget_pool(homepage, {
+      _obj_class: '<%= panel_obj_class_name %>',
+      headline: 'Excepteur sint occaecat cupidatat',
+      main_content: {
+        'list' => [
+          { widget: text_widget2_id },
+        ]
+      },
     })
 
     add_widget(homepage, '<%= example_widget_attribute %>', {
@@ -25,7 +55,7 @@ class CreateAccordionWidgetExample < ::RailsConnector::Migration
 
   private
 
-  def add_widget_to_widget_pool(obj, attribute, widget_params)
+  def add_widget_to_widget_pool(obj, widget_params)
     workspace_id = RailsConnector::Workspace.current.id
     obj_params = RailsConnector::CmsRestApi.get("workspaces/#{workspace_id}/objs/#{obj.id}")
     widget_id = RailsConnector::BasicObj.generate_widget_pool_id
