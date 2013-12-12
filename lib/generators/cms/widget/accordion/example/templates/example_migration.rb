95dd7ad6af760f86922f546ed76913e4fc587255
Falk Köppe <falk.koeppe@infopark.de>
2013-12-12 10:07:19 +0100

    update example migration after widget reform

--- lib/generators/cms/widget/accordion/example/templates/example_migration.rb
+++ lib/generators/cms/widget/accordion/example/templates/example_migration.rb
@@ -2,24 +2,41 @@ class CreateAccordionWidgetExample < ::RailsConnector::Migration
   def up
     homepage = Obj.find_by_path('<%= example_cms_path %>')
 
-    widget = add_widget(homepage, '<%= example_widget_attribute %>', {
-      _obj_class: '<%= obj_class_name %>'
-    })
-
-    widget = Obj.find(widget['id'])
-
-    add_widget(widget, '<%= panel_attribute %>', {
+    panel_widget1_id = add_widget_to_widget_pool(homepage, '<%= panel_attribute %>', {
       _obj_class: '<%= panel_obj_class_name %>',
       headline: 'Panel 1',
     })
 
-    add_widget(widget, '<%= panel_attribute %>', {
+    panel_widget2_id = add_widget_to_widget_pool(homepage, '<%= panel_attribute %>', {
       _obj_class: '<%= panel_obj_class_name %>',
       headline: 'Panel 2',
+    })
+
+    add_widget(homepage, '<%= example_widget_attribute %>', {
+      _obj_class: '<%= obj_class_name %>',
+      panels: {
+        'list' => [
+          'widget' => panel_widget1_id,
+          'widget' => panel_widget2_id,
+        ]
+      }
     })
   end
 
   private
+
+  def add_widget_to_widget_pool(obj, attribute, widget_params)
+    workspace_id = RailsConnector::Workspace.current.id
+    obj_params = RailsConnector::CmsRestApi.get("workspaces/#{workspace_id}/objs/#{obj.id}")
+    widget_id = RailsConnector::BasicObj.generate_widget_pool_id
+
+    params = {}
+    params['_widget_pool'] = { widget_id => widget_params }
+
+    update_obj(obj_params['id'], params)
+
+    widget_id
+  end
 
   def add_widget(obj, attribute, widget_params)
     workspace_id = RailsConnector::Workspace.current.id
