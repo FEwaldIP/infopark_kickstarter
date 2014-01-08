215c760e1b181b7152e759a0a98bd214dc00fd8a
Falk Köppe <falk.koeppe@infopark.de>
2014-01-08 08:19:42 +0100

    update profile page

--- lib/generators/cms/component/profile_page/example/example_generator.rb
+++ lib/generators/cms/component/profile_page/example/example_generator.rb
@@ -7,26 +7,14 @@ module Cms
 
           source_root File.expand_path('../../templates', __FILE__)
 
-          argument :cms_path,
-            type: :string,
-            default: nil,
-            desc: 'CMS parent path where the example profile should be placed under.',
-            banner: 'LOCATION'
-
           def create_example
-            migration_template('example_migration.rb', 'cms/migrate/create_profile_page_example.rb')
+            migration_template('migration.rb', 'cms/migrate/profile_page_example.rb')
           end
 
           def notice
             if behavior == :invoke
               log(:migration, 'Make sure to run "rake cms:migrate" to apply CMS changes.')
             end
-          end
-
-          private
-
-          def obj_class_name
-            'ProfilePage'
           end
         end
       end
