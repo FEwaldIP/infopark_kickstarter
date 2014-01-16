178e116c1c611d0861ced75c7fe4e3888e7f06f0
Falk Köppe <falk.koeppe@infopark.de>
2014-01-16 16:41:53 +0100

    bugfix: small images could not be uploaded and media objects did not work as expected

--- lib/generators/cms/widget/text_image/example/templates/migration.rb
+++ lib/generators/cms/widget/text_image/example/templates/migration.rb
@@ -4,7 +4,7 @@ class TextImageWidgetExample < RailsConnector::Migration
   def up
     homepage = Obj.find_by_path('<%= example_cms_path %>')
 
-    asset_url = 'http://lorempixel.com/150/120/abstract'
+    asset_url = 'http://lorempixel.com/400/120/abstract'
 
     asset = create_obj({
       _obj_class: 'Image',
@@ -21,7 +21,14 @@ class TextImageWidgetExample < RailsConnector::Migration
         est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
         diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet.',
     })
   end
 
